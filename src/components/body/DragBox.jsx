2c1db265da3bd0f6caf223df2a3dc8550af25e94
whddnjs1715 <whddnjs1715@gmail.com>
2022-02-15 11:40:03 +0900

    DragBox add styled-components #4

--- src/components/body/DragBox.jsx
+++ src/components/body/DragBox.jsx
@@ -1,11 +1,42 @@
 import React from "react";
+import styled from "styled-components";
 
 const DraggableBox = () => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <Draggable>
+      <Button>OpenButton</Button>
+      <Box>
+        <DragContent></DragContent>
+        <Button>CloseButton</Button>
+      </Box>
+    </Draggable>
   );
 };
+
+const Draggable = styled.div``;
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: lightgrey;
+  border-radius: 10px;
+  border: 3px solid grey;
+  position: absolute;
+  cursor: move;
+  top: 150px;
+  left: 50px;
+`;
+
+const DragContent = styled.div``;
+
+const Button = styled.button`
+  border-radius: 10px;
+  border: 3px solid black;
+  margin-left: 10px;
+  width: 100px;
+  height: 30px;
+  margin-top: 10px;
+  margin: 0 auto;
+  margin-top: 10px;
+`;
 
 export default DraggableBox;
