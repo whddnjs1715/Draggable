5518c37c974df6e49683cac2f80c34ae09549552
whddnjs1715 <whddnjs1715@gmail.com>
2022-02-15 11:47:17 +0900

    Create Draggable Action in DragBox #8

--- src/components/body/DragBox.jsx
+++ src/components/body/DragBox.jsx
@@ -3,11 +3,51 @@ import styled from "styled-components";
 
 const DraggableBox = () => {
   const [modal, setModal] = useState(true);
+  const [diffX, setDiffX] = useState(0);
+  const [diffY, setDiffY] = useState(0);
+  const [dragging, setDragging] = useState(false);
+  const [style, setStyle] = useState({});
+
+  const dragStart = (e) => {
+    setDiffX(e.screenX - e.currentTarget.getBoundingClientRect().left);
+    setDiffY(e.screenY - e.currentTarget.getBoundingClientRect().top);
+    setDragging(true);
+  };
+
+  const dragged = (e) => {
+    if (dragged) {
+      const left = e.screenX - diffX;
+      const top = e.screenY - diffY;
+
+      setStyle({
+        styles: {
+          left: left,
+          top: top,
+        },
+      });
+    }
+  };
+
+  const dragEnd = (e) => {
+    setDragging(false);
+  };
+
   return (
     <Draggable>
       <Button onClick={() => setModal(true)}>OpenButton</Button>
       {modal === true ? (
-        <Box>
+        <Box
+          style={style.styles}
+          onMouseDown={(e) => {
+            dragStart(e);
+          }}
+          onMouseMove={(e) => {
+            dragged(e);
+          }}
+          onMouseUp={(e) => {
+            dragEnd(e);
+          }}
+        >
           <DragContent></DragContent>
           <Button>CloseButton</Button>
         </Box>
